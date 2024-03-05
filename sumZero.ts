d5c1c3193ab2ac6ea3bbe89c631aaad7e77edbda
anfnamazi <anf.namazi@gmail.com>
2024-03-05 22:59:14 +0330

    feat: add function sum zero return two number that sum of them equal zero

---
+++ sumZero.ts
@@ -0,0 +1,21 @@
+function sumZero(sortedArr: number[]): [number, number] | undefined {
+  for (let i = 0; i < sortedArr.length; i++) {
+    if (sortedArr[i] >= 0) {
+      return;
+    }
+    let min = 0;
+    let max = sortedArr.length - 1;
+    while (min <= max) {
+      let mid = Math.floor((max + min) / 2);
+      if (sortedArr[mid] === Math.abs(sortedArr[i])) {
+        return [sortedArr[i], sortedArr[mid]];
+      } else if (Math.abs(sortedArr[i]) > sortedArr[mid]) {
+        min = mid + 1;
+      } else {
+        max = mid - 1;
+      }
+    }
+  }
+}
+
+console.log(sumZero([-3, -1, 0, 2, 3]));
