96f06969cddcb299916d20480d2eb129d8f11c21
anfnamazi <anf.namazi@gmail.com>
2024-03-30 05:17:40 +0330

    refactor: add generic type to quick sort

--- sort/quickSort.ts
+++ sort/quickSort.ts
@@ -1,13 +1,13 @@
 namespace quickSort {
-  export function quickSort(arr: number[]): number[] {
+  export function quickSort<T>(arr: T[]): T[] {
     if (arr.length <= 1) {
       return arr;
     }
 
     const randomIndex = Math.floor(Math.random() * arr.length);
 
-    const left: number[] = [];
-    const right: number[] = [];
+    const left: T[] = [];
+    const right: T[] = [];
 
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] > arr[randomIndex]) {
@@ -17,8 +17,9 @@ namespace quickSort {
       }
     }
 
-    return quickSort(left).concat(quickSort(right));
+    return quickSort<T>(left).concat(quickSort<T>(right));
   }
 
-  console.log(quickSort([5, 4, 3, 2, 1]));
+  console.log(quickSort<number>([5, 4, 3, 2, 1]));
+  console.log(quickSort<string>(["f", "e", "d", "c", "a"]));
 }
