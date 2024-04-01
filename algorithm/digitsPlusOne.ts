212ee2009d5b04ada8a9df15cfe360a5948ec627
anfnamazi <anf.namazi@gmail.com>
2024-04-01 13:34:16 +0330

    feat: add plus one to digits array

---
+++ algorithm/digitsPlusOne.ts
@@ -0,0 +1,14 @@
+function plusOne(digits: number[]): number[] {
+  let str = digits.join("");
+  let num = BigInt(str) + 1n;
+  let newStr = num.toString();
+  let newDigits = [];
+  for (let i of newStr) {
+    newDigits.push(+i);
+  }
+
+  return newDigits;
+}
+
+console.log(plusOne([4, 3, 2, 1]));
+console.log(plusOne([9, 9]));
