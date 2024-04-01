39709bbd9316e517281183eb57413c4d8f15602e
anfnamazi <anf.namazi@gmail.com>
2024-04-01 13:35:45 +0330

    refactor: add namespace to digits plus one

--- algorithm/digitsPlusOne.ts
+++ algorithm/digitsPlusOne.ts
@@ -1,14 +1,16 @@
-function plusOne(digits: number[]): number[] {
-  let str = digits.join("");
-  let num = BigInt(str) + 1n;
-  let newStr = num.toString();
-  let newDigits = [];
-  for (let i of newStr) {
-    newDigits.push(+i);
+namespace digitsArray {
+  function plusOne(digits: number[]): number[] {
+    let str = digits.join("");
+    let num = BigInt(str) + 1n;
+    let newStr = num.toString();
+    let newDigits = [];
+    for (let i of newStr) {
+      newDigits.push(+i);
+    }
+
+    return newDigits;
   }
 
-  return newDigits;
+  console.log(plusOne([4, 3, 2, 1]));
+  console.log(plusOne([9, 9]));
 }
-
-console.log(plusOne([4, 3, 2, 1]));
-console.log(plusOne([9, 9]));
