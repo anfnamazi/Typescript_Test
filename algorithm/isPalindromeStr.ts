20b91cbea60feb68b730119274248c0bb5fed05f
anfnamazi <anf.namazi@gmail.com>
2024-04-06 04:22:12 +0330

    feat: another solution to palindrome str

--- algorithm/isPalindromeStr.ts
+++ algorithm/isPalindromeStr.ts
@@ -10,9 +10,20 @@ namespace validPalindrome {
         reverseStr = c + reverseStr;
       }
     }
-    return reverseStr === str;
+    return true;
   }
 
-  console.log(isPalindrome("race a car"));
+  //other solution
+  function isPalindrome2(s: string): boolean {
+    s = s.replaceAll(/[^a-zA-Z0-9]/g, "");
+    for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+      if (s[i].toLowerCase() !== s[j].toLowerCase()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  console.log(isPalindrome2("race a car"));
   console.log(isPalindrome("A man, a plan, a canal: Panama"));
 }
