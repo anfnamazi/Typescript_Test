11a69e3e83f084fc3843634b9ecd80c3c0e2abca
anfnamazi <anf.namazi@gmail.com>
2024-04-07 15:11:11 +0330

    refactor: twoSum algorithm add namespace

--- algorithm/twoSum.ts
+++ algorithm/twoSum.ts
@@ -1,10 +1,14 @@
-function twoSum(nums: number[], target: number): number[] {
-    const map = new Map()
+namespace twoSum {
+  function twoSum(nums: number[], target: number): number[] {
+    const map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(target - nums[i])) { return [map.get(target - nums[i]), i] }
-        map.set(nums[i],i)
+      if (map.has(target - nums[i])) {
+        return [map.get(target - nums[i]), i];
+      }
+      map.set(nums[i], i);
     }
-    return [-1, -1]
-};
+    return [-1, -1];
+  }
 
-console.log(twoSum([2,7,11,15],9))
+  console.log(twoSum([2, 7, 11, 15], 9));
+}
