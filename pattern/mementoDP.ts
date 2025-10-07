516ee84c5464be9aa1087e639f4b53fe3b5613c2
mojtaban <mojtaba.n@behinrahkar.com>
2025-10-07 14:23:32 +0330

    feat: add memento design pattern

---
+++ pattern/mementoDP.ts
@@ -0,0 +1,67 @@
+/**
+ * ### Core Concepts
+ * Originator: The object whose state needs to be saved/restored.
+ * Memento: A snapshot of the Originator’s state.
+ * Caretaker: Manages the history of Mementos.
+ *
+ * ### Benefit
+ * Implementing undo/redo
+ * Saving checkpoints in games
+ * Preserving state before risky operations
+ */
+
+namespace mementoDP {
+  class Memento {
+    constructor(private state: string) {}
+
+    getState() {
+      return this.state;
+    }
+  }
+
+  class TextEditor {
+    private content: string = "";
+
+    write(text: string) {
+      this.content += text;
+    }
+
+    getContent() {
+      return this.content;
+    }
+
+    save() {
+      return new Memento(this.content);
+    }
+
+    restore(memento: Memento) {
+      this.content = memento.getState();
+    }
+  }
+
+  class History {
+    private mementos: Memento[] = [];
+
+    push(memento: Memento) {
+      this.mementos.push(memento);
+    }
+
+    pop() {
+      return this.mementos.pop();
+    }
+  }
+
+  // Usage
+  const textEditor = new TextEditor();
+  const history = new History();
+
+  textEditor.write("Hello,");
+  const memento = textEditor.save();
+  history.push(memento);
+
+  textEditor.write("world!");
+  console.log(textEditor.getContent());
+
+  textEditor.restore(history.pop()!);
+  console.log(textEditor.getContent());
+}
