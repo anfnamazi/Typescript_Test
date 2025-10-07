22a77f76124cee11af58860bc7b7ab8e863a9aab
mojtaban <mojtaba.n@behinrahkar.com>
2025-10-07 10:34:47 +0330

    feat: add strategy design pattern

---
+++ pattern/strategyDP.ts
@@ -0,0 +1,49 @@
+namespace strategyDP {
+  interface IPaymentStrategy {
+    pay(amount: number): void;
+  }
+
+  class CreditCardPayment implements IPaymentStrategy {
+    pay(amount: number): void {
+      console.log("Paying " + amount + " using Credit Card");
+    }
+  }
+
+  class PayPalPayment implements IPaymentStrategy {
+    pay(amount: number): void {
+      console.log("Paying " + amount + " using PayPal");
+    }
+  }
+
+  class BitcoinPayment implements IPaymentStrategy {
+    pay(amount: number): void {
+      console.log("Paying " + amount + " using Bitcoin");
+    }
+  }
+
+  class ShoppingCart {
+    private paymentStrategy: IPaymentStrategy;
+
+    constructor(paymentStrategy: IPaymentStrategy) {
+      this.paymentStrategy = paymentStrategy;
+    }
+
+    setPaymentStrategy(paymentStrategy: IPaymentStrategy) {
+      this.paymentStrategy = paymentStrategy;
+    }
+
+    checkout(amount: number) {
+      this.paymentStrategy.pay(amount);
+    }
+  }
+
+  // Usage
+  const cart = new ShoppingCart(new CreditCardPayment());
+  cart.checkout(100); // Paying 100 using Credit Card
+
+  cart.setPaymentStrategy(new PayPalPayment());
+  cart.checkout(200); // Paying 200 using PayPal
+
+  cart.setPaymentStrategy(new BitcoinPayment());
+  cart.checkout(300); // Paying 300 using Bitcoin
+}
