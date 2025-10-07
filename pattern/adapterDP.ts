86852dc28e26900e38f4d13c2dab00097824eb2f
mojtaban <mojtaba.n@behinrahkar.com>
2025-10-07 15:52:39 +0330

    feat: add adapter design pattern

---
+++ pattern/adapterDP.ts
@@ -0,0 +1,40 @@
+/**
+ * ### Core Concepts
+ * Convert one interface to another expected by the client.
+ * Promote code reusability and flexibility.
+ * Useful when integrating legacy code or third-party libraries.
+ *
+ * ### Benefits
+ * Integrating new code with legacy systems.
+ * Wrapping third-party libraries to match your app’s interface.
+ * Bridging incompatible interfaces in large systems.
+ */
+
+namespace adapterDP {
+  class FahrenheitSensor {
+    constructor(private temperature: number) {}
+    getTemperature() {
+      return this.temperature;
+    }
+  }
+
+  interface CelsiusSensor {
+    getTemperatureInCelsius(): number;
+  }
+
+  class TemperatureAdapter implements CelsiusSensor {
+    constructor(private readonly sensor: FahrenheitSensor) {}
+
+    getTemperatureInCelsius(): number {
+      const fahrenheit = this.sensor.getTemperature();
+
+      return ((fahrenheit - 32) * 5) / 9;
+    }
+  }
+
+  // Usage
+  const fahrenheitSensor = new FahrenheitSensor(98.6);
+
+  const adapter = new TemperatureAdapter(fahrenheitSensor);
+  console.log(adapter.getTemperatureInCelsius());
+}
